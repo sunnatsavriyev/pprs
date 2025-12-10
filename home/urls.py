575620a09defa82fb0669cf8924a18621d3dbb52
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2025-12-10 10:02:59 +0500

    first commit

---
+++ home/urls.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+# Swagger uchun schema view
+schema_view = get_schema_view(
+   openapi.Info(
+      title="PPR API",
+      default_version='v1',
+      description="PPR API documentation with Swagger",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('ppradmin/', admin.site.urls),
+    path('api/', include('ppr.urls')),  # appingiz API url-lari
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls')), 
+    
+    
+    # Swagger URL-lari
+    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
