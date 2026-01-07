7065f8944f22da6fc771251c258283d61aba3fe9
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2026-01-07 14:04:19 +0500

    password for update2

--- ppr/serializers.py
+++ ppr/serializers.py
@@ -66,7 +66,7 @@ class UserTuzilmaSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
         required=False,
-        allow_blank=False,
+        allow_blank=True,
         min_length=6
     )
 
